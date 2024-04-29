bd466b94d7d5db2e11557f50a5de413ed00b9313
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-04-29 15:02:55 +0300

    extract urls

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(url) {
   try {
     const urlObj = new URL(url);
@@ -7,7 +9,23 @@ export function normalizeURL(url) {
     }
     return fullPath;
   } catch (error) {
-    throw new Error("bad url")
+    throw new Error("bad url");
   }
 }
 
+export function getURLsFromHTML(htmlBody, baseURL) {
+  const anchors = new JSDOM(htmlBody).window.document.querySelectorAll("a");
+  const links = Array.from(anchors)
+    .filter((a) => a.hasAttribute("href"))
+    .map((a) => {
+      const href = a.getAttribute("href");
+      try {
+        const url = new URL(href, baseURL).href;
+        return url;
+      } catch (err) {
+        return false;
+      }
+    })
+    .filter(Boolean);
+  return links
+}
--- crawl.test.js
+++ crawl.test.js
@@ -1,27 +1,49 @@
-import { describe, it } from "node:test";
 import assert from "node:assert";
+import { describe, it } from "node:test";
 
-import { normalizeURL } from "./crawl.js";
+import { getURLsFromHTML, normalizeURL } from "./crawl.js";
 
 describe(normalizeURL.name, () => {
-  it('normal flow', () => {
+  it("normal flow", () => {
     const cases = [
-      {input: "https://blog.boot.dev/path/", result: "blog.boot.dev/path", },
-      {input: "https://blog.boot.dev/path", result: "blog.boot.dev/path", },
-      {input: "http://BLOG.boot.dev/path/", result: "blog.boot.dev/path", },
-      {input: "http://blog.boot.dev/path", result: "blog.boot.dev/path", },
-    ]
+      { input: "https://blog.boot.dev/path/", result: "blog.boot.dev/path" },
+      { input: "https://blog.boot.dev/path", result: "blog.boot.dev/path" },
+      { input: "http://BLOG.boot.dev/path/", result: "blog.boot.dev/path" },
+      { input: "http://blog.boot.dev/path", result: "blog.boot.dev/path" },
+    ];
     for (const opt of cases) {
-      assert.equal(normalizeURL(opt.input), opt.result)
+      assert.equal(normalizeURL(opt.input), opt.result);
     }
-  })
-  it('error flow', () => {
+  });
+  it("error flow", () => {
     const cases = [
-      {input: "fakeurl", message: "bad url", },
-      {input: "httpsftp?fakeurl", message: "bad url", },
-    ]
+      { input: "fakeurl", message: "bad url" },
+      { input: "httpsftp?fakeurl", message: "bad url" },
+    ];
     for (const opt of cases) {
-      assert.throws(() => normalizeURL(opt.input), Error, opt.message)
+      assert.throws(() => normalizeURL(opt.input), Error, opt.message);
     }
-  })
-})
+  });
+});
+
+describe(getURLsFromHTML.name, () => {
+  const html = `
+  <html>
+    <body>
+        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        <a href="/articles"><span>Articles</span></a>
+        <a href="/shop"><span>Shop</span></a>
+    </body>
+  </html>
+  `;
+  const base = "https://blog.boot.dev";
+  it("extract all links", () => {
+    const urls = getURLsFromHTML(html, base);
+    assert.equal(urls.length, 3);
+    assert.deepEqual(urls, [
+      "https://blog.boot.dev/",
+      "https://blog.boot.dev/articles",
+      "https://blog.boot.dev/shop",
+    ]);
+  });
+});
