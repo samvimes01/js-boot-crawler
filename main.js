9c752cf97dc7636c0814934d9fb240c251b685b2
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-04-29 15:12:40 +0300

    start main process

---
+++ main.js
@@ -0,0 +1,14 @@
+function main() {
+  if (process.argv.length < 3) {
+    console.error("missing base url argument");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.error("wrong number of arguments");
+    process.exit(2);
+  }
+  const baseUrl = process.argv[2];
+  console.log("started crawling urls at", baseUrl);
+}
+
+main();
